6cda36b2aee5222b85f2dc39320c6eb90344293c
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-11 18:58:24 -0300

    Create home.php

---
+++ view/home.php
@@ -0,0 +1,7 @@
+<section id="sectionHome">
+     <h1>Bienvenue sur le site</h1>
+     <div class="homebox">
+         <a href="#sectionBlog" id="buttonChapterAll">Listes des chapitres</a>
+         <a href="view/about.php" id="buttonBiographie">Biographie</a>
+     </div>
+ </section>
