5e07b2e813df71fc0c3d9d92858b7defb85d49dd
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-11 18:53:37 -0300

    Create footer.php

---
+++ view/footer.php
@@ -0,0 +1,33 @@
+<!-- Section Footer -->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+ <script>
+     tinymce.init({
+       selector: 'textarea',
+       plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+       toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+       toolbar_mode: 'floating',
+       tinycomments_mode: 'embedded',
+       tinycomments_author: 'Author name'
+     });
+   </script>
+  <footer>
+       <div class="footerNav">
+           <h4>Menu</h4>
+           <ul>
+             <li><a href="../index.php" >Accueil</a></li>
+             <li><a href="../view/about.php" >A Propos</a></li>
+             <li><a href="../view/contact.php" >Contact</a></li>
+           </ul>
+       </div>
+       <div class="footerAdmin">
+           <h4>Administrateur</h4>
+           <ul>
+             <li><a href="" >Se Connecter</a></li>
+           </ul>
+       </div>
+  </footer>
+
+  <!-- End Section Footer -->
