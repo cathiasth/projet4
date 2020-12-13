f77242af3c50f1c72c6c78f0f381b529da74d010
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-13 15:37:44 -0300

    Create config.php

---
+++ controller/config.php
@@ -0,0 +1,9 @@
+<?php
+
+ //Nos constantes
+ const HOST = '';
+ const DB_NAME = '';
+ const CHARSET = 'utf8';
+ const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
+ const DB_USER = '';
+ const DB_PASS = '';
