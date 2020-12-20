b4f21a0c53b255548d539a1c4c0d497733a0b7f5
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-20 18:30:33 -0300

    Ajout de 2 fonctions getPost et addPost
    getPost permet de récupérer les données d'un chapitre sélectionné et addPost permet de rajouter dans la base de données un nouveau chapitre.

--- model/PostManager.php
+++ model/PostManager.php
@@ -11,7 +11,24 @@ class PostManager
         $request = $db->query('SELECT id, title, content, creation_date FROM posts ORDER BY id DESC');
         return $request;
     }
-    
+    // Renvoie les informations d'un chapitre
+    public function getPost($postId)
+    {   
+        $db = $this->dbConnect();
+        $request = $db->prepare('SELECT id, title, content, creation_date FROM posts WHERE id = ?');
+        $request->execute(array($postId));
+        $post = $request->fetch();
+        return $post;
+    }
+  
+    public function addPost($title, $content)
+    {   
+        $db = $this->dbConnect();
+        $request = $db->prepare('INSERT INTO posts (title, content, creation_date) VALUES (? , ? , NOW())');
+        $request->execute(array($title,$content));
+        $post = $request->fetch();
+        return $post;
+    }
     protected function dbConnect()
     {
         $db = new \PDO('mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET.';', DB_USER, DB_PASS);
