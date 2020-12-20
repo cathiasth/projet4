9e899e6f0de6c8e79313fb47ef06439894bc92a1
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-20 18:51:41 -0300

    Ajout getComments, addComment et flagComment
    Ajout de 3 fonctions :
    - getComments permet de récupérer les commentaires d'un chapitre sélectionné depuis la base de données
    - addComment permet d'ajouter un commentaire d'un chapitre sélectionné dans la base de données
    - flagComment permet de rajouter le signalement d'un commentaire précis en le modifiant dans la base de données (par défaut signalement est un boleen = 0).

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -4,6 +4,28 @@ namespace App\model;
 
 class CommentManager
 {
+    public function getComments($postId)
+    { 
+        $db = $this->dbConnect();
+        $comments = $db->prepare('SELECT id, author, comment_author, comment_date, flag FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments->execute(array($postId));
+        return $comments;
+    }
+  
+    public function addComment($author, $commentAuthor, $postId)
+    {
+        $db = $this->dbConnect();
+        $request = $db->prepare('INSERT INTO comments (author, comment_author, comment_date, post_id) VALUES (:author, :comment_author, NOW(), :post_id)');
+        $request->execute(array(':author' => $author, ':comment_author' => $commentAuthor,':post_id' => $postId));
+    }
+  
+    public function flagComment($id)
+    {
+        $db = $this->dbConnect();
+        $request = $db->prepare('UPDATE comments SET flag = ? WHERE id = ?');
+        $request->execute(array(1, $id));
+    }
+    
     protected function dbConnect()
     {
         $db = new \PDO('mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET.';', DB_USER, DB_PASS);
