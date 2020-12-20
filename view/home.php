9c255e69df95d970eb8f11bf4650747a6d683274
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-20 19:12:46 -0300

    Mise à jour lien dans la vue

--- view/home.php
+++ view/home.php
@@ -1,7 +1,7 @@
 <section id="sectionHome">
-     <h1>Bienvenue sur le site</h1>
-     <div class="homebox">
-         <a href="#sectionBlog" id="buttonChapterAll">Listes des chapitres</a>
-         <a href="view/about.php" id="buttonBiographie">Biographie</a>
-     </div>
- </section>
+    <h1>Bienvenue sur le site</h1>
+    <div class="homebox">
+        <a href="#sectionBlog" id="buttonChapterAll">Listes des chapitres</a>
+        <a href="/about/" id="buttonBiographie">Biographie</a>
+    </div>
+</section>
