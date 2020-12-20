56f6e1f14f015208f58c8a5723d45cb0b1f8bc5b
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-20 19:18:04 -0300

    Mise à jour des liens de la vue

--- view/footer.php
+++ view/footer.php
@@ -1,33 +1,40 @@
 <!-- Section Footer -->
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
- <script>
-     tinymce.init({
-       selector: 'textarea',
-       plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-       toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-       toolbar_mode: 'floating',
-       tinycomments_mode: 'embedded',
-       tinycomments_author: 'Author name'
-     });
-   </script>
-  <footer>
-       <div class="footerNav">
-           <h4>Menu</h4>
-           <ul>
-             <li><a href="../index.php" >Accueil</a></li>
-             <li><a href="../view/about.php" >A Propos</a></li>
-             <li><a href="../view/contact.php" >Contact</a></li>
-           </ul>
-       </div>
-       <div class="footerAdmin">
-           <h4>Administrateur</h4>
-           <ul>
-             <li><a href="" >Se Connecter</a></li>
-           </ul>
-       </div>
-  </footer>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/6059c1f543.js" crossorigin="anonymous"></script>
+<script>
+    tinymce.init({
+      selector: 'textarea',
+      toolbar_mode: 'floating',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: 'Author name'
+    });
+  </script>
+ <footer>
+      <div class="footlinks">
+          <div class="footerNav">
+              <ul>
+                <li><a href="/home/" >Accueil</a></li>
+                <li><a href="/about/">A Propos</a></li>
+                <li><a href="/contact/" >Contact</a></li>
+              </ul>
+          </div>
+          <div class="footerAdmin">
+              <ul>
+                <li><a href="/login/" >Se Connecter</a></li>
+                <li><a href="/mentions-propres-au-site/">Mentions propres au site</a></li>
+                <li><a href="/mentions-relatives-utilisations-cookies/">Mentions relatives à l'utilisations des cookies</a></li>
+                <li><a href="/mentions-relatives-utilisations-donnees-personnelles/">Mentions relatives à l'utilisations des données personnelles</a></li>
+              </ul>
+          </div>
+      </div>
+      <div class="copyright">
+        <p>
+          Ce site a été créé dans le cadre d'une formation.
+        </p>
+      </div>
+ </footer>
 
-  <!-- End Section Footer -->
+ <!-- End Section Footer -->
