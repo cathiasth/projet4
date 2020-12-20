7b210e20153a2aa95e76383f3d1838bd6c6c1061
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-20 19:23:40 -0300

    Création de la vue À Propos

---
+++ view/about.php
@@ -0,0 +1,59 @@
+<?php $title = 'Jean Forteroche - A Propos'; ?>
+ <?php require("../view/header.php"); ?>
+            <section id="sectionBiographie">
+                <div class="container text-center">
+                    <h1>À propos de moi</h1>
+                    <div class="biographie-text">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis iaculis nibh,
+                          quis ornare libero porta eu. In a vestibulum diam. Fusce et condimentum arcu. Donec 
+                          venenatis justo at nibh elementum dictum. Sed eu ultrices diam. Suspendisse vitae eros
+                          pellentesque, tincidunt libero vel, ornare nibh. Pellentesque egestas, urna condimentum
+                          imperdiet gravida, quam risus rhoncus ligula, et suscipit lectus eros et eros. Vivamus
+                          sodales, diam quis hendrerit hendrerit, ipsum dolor maximus sem, non imperdiet eros
+                          velit mattis neque. Pellentesque efficitur sit amet justo ac pellentesque. Nam sodales,
+                          sem vitae mollis porttitor, felis nulla pharetra ex, a vulputate leo purus a diam. 
+                          Maecenas erat est, consequat at nunc vel, sodales faucibus velit. Suspendisse 
+                          consectetur sed diam ac sodales.</p><br/>
+
+                          <p>Mauris porta ante ultrices risus ultricies cursus eu eu nibh. Sed ut lacinia neque. 
+                          Mauris placerat est quis ex varius, ut euismod elit elementum. Quisque in metus mi. 
+                          Quisque blandit erat quis velit molestie, non rutrum erat gravida. Integer placerat 
+                          lectus a imperdiet placerat. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                          Nullam convallis est eu orci finibus, quis blandit velit elementum. Nunc varius eget sapien 
+                          sit amet posuere. Pellentesque lectus elit, varius ut arcu et, sollicitudin ultricies odio. 
+                          Nunc nec vestibulum augue. Quisque sit amet nunc dui. Maecenas ut urna in purus mollis 
+                          pellentesque at et dolor. Aliquam erat volutpat. Vestibulum vel sodales diam. Ut maximus 
+                            dui augue, ac eleifend dolor auctor sed.</p><br/>
+
+                          <p>Vivamus et semper erat. Maecenas scelerisque et enim id fermentum. Morbi a finibus neque. 
+                          Aenean sem ipsum, tempus at magna vel, dapibus placerat mauris. Mauris nec tincidunt sapien. 
+                          Aliquam porta in libero eget hendrerit. Maecenas ut nunc et felis vulputate varius. 
+                          Vestibulum a finibus est. Mauris pretium dignissim ipsum, mollis vestibulum dolor tincidunt 
+                          eget. Nullam quis metus lorem. Duis nisl nulla, lacinia ut semper ac, ornare sit amet justo. 
+                          Curabitur quis mattis metus. Pellentesque diam eros, euismod id bibendum non, commodo a odio. 
+                            Maecenas rhoncus magna et dignissim faucibus.</p><br/>
+
+                          <p>Morbi posuere magna orci, viverra bibendum est viverra vel. Sed venenatis odio nec nisl auctor, 
+                          in lacinia nunc pharetra. Cras aliquam eget nulla eget pellentesque. Fusce magna arcu, eleifend 
+                          vestibulum eleifend a, posuere in mi. Suspendisse gravida dignissim metus, sed dignissim lectus 
+                          imperdiet at. Cras mauris velit, varius ut urna quis, molestie rhoncus nunc. Donec tempus lorem 
+                          gravida sollicitudin tempor. Curabitur finibus quam at venenatis sollicitudin. Vestibulum sit 
+                            amet dolor venenatis, condimentum purus non, mattis diam.</p><br/>
+
+                          <p>Phasellus posuere nulla non nisi rutrum efficitur. Mauris eu turpis vel ligula maximus mattis et
+                          a ex. Ut vitae ligula lobortis, rhoncus leo nec, efficitur augue. Aenean fringilla, sapien eu 
+                          consectetur accumsan, tortor purus hendrerit est, non finibus magna urna at nunc. Integer ac dui 
+                          neque. Nunc ullamcorper porta ipsum, eget dapibus felis. Vestibulum luctus dolor eget ligula 
+                          finibus mattis. In risus lorem, ultricies ut mollis ac, porta in massa. Aliquam vitae rhoncus sem.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            
+        </body>
+      <?php require("../view/footer.php"); ?>
+
+<?php require('../view/template.php'); ?>
+
