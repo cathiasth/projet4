83f1297cd4647ce1d50f59f412c17a8684309a0b
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-21 11:09:45 -0300

    Derniers ajouts des routes

--- controller/Router.php
+++ controller/Router.php
@@ -38,15 +38,81 @@ class Router
                         header('Location:/home/');
                     }
                 }
+            elseif($_GET['route'] === 'editPost'){
+                    if(isset($_SESSION["email"])){
+                        $this->backController->editPost($_GET['postId']);
+                    }else{
+                        header('Location:/home/');
+                    }
+                }
+                elseif($_GET['route'] == 'updatePost'){
+                    if(isset($_SESSION["email"])){
+                        $this->frontController->updatePost($_POST['title'], $_POST['content'],$_GET['postId']);
+                    }else{
+                        header('Location: /home/');
+                    }
+                }
+                elseif($_GET['route'] === 'deletePost'){
+                    if(isset($_SESSION["email"])){
+                        $this->backController->deletePost($_GET['postId']);
+                    }else{
+                        header('Location: /home/');
+                    }
+                }
              elseif($_GET['route'] === 'addComment'){
                     $this->frontController->addComment($_POST['author'], $_POST['comment_author'], $_GET['postId']);
                 }
              elseif($_GET['route'] === 'flagComment'){
                     $this->frontController->flagComment($_GET['id'], $_GET['postId']);
                 }
-             elseif($_GET['route'] === 'login'){
+             elseif($_GET['route'] === 'flagOffComment'){
+                    if(isset($_SESSION["email"])){
+                        $this->backController->flagOffComment($_GET['id']);
+                    }else{
+                        header('Location: /home/');
+                    }
+                }
+                elseif($_GET['route'] === 'deleteComment'){
+                    if(isset($_SESSION["email"])){
+                        $this->backController->deleteComment($_GET['id'], $_GET['postId']);
+                    }else{
+                        header('Location: /home/');
+                    }
+                }
+                elseif($_GET['route'] === 'register'){
+                }
+                elseif($_GET['route'] === 'login'){
                     $this->frontController->login($_GET['email']);
                 }
+                elseif($_GET['route'] === 'profil'){
+                    if(isset($_SESSION["email"])){
+                        $this->frontController->profil();
+                    }else{
+                        header('Location: /home/');
+                    }
+                }
+                elseif($_GET['route'] === 'logout'){
+                    $this->backController->logout();
+                }
+                elseif($_GET['route'] === 'about'){
+                    require('../view/about.php');
+                }
+                elseif($_GET['route'] === 'contact'){
+                    require('../view/contact.php');
+                }
+                elseif($_GET['route'] === 'mentionsPropresAuSite'){
+                    require('../view/mentions_propres_au_site.php');
+                }
+                elseif($_GET['route'] === 'mentionsRelativesUtilisationCookies'){
+                    require('../view/mentions_relatives_utilisations_cookies.php');
+                }
+                elseif($_GET['route'] === 'mentionsRelativesUtilisationsDonneesPersonnelles'){
+                    require('../view/mentions_relatives_utilisations_donnees_personnelles.php');
+                }
+                else{
+                    $this->errorController->errorNotFound();
+                }
+             }
              else{
                 $this->frontController->getPosts();
             }
