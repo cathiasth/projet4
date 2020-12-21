54be9f900e746eb6b5f4bbd73c9662e544f16f4b
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-21 10:55:07 -0300

    Dernières modifications
    Ajout 5 fonctions :
    - editPost permet d'afficher les éléments d'un chapitre sélectionné
    - deletePost permet de supprimer un chapitre
    - deleteComment permet supprimer tous les commentaires d'un chapitre
    - flagOffComment permet d'enlever le signalement d'un commentaire
    - logout permet de se déconnecter l'utilisateur

--- controller/BackController.php
+++ controller/BackController.php
@@ -12,4 +12,36 @@ class BackController extends Controller
         header('Location: /home/');
     }
   
+  public function editPost($postId)
+  {
+        $post = $this->postManager->editPost($postId);
+        require '../view/edit_post.php';
+  }
+  
+  public function deletePost($postId)
+  {
+        $this->postManager->deletePost($postId);
+        $this->commentManager->deleteComments($postId);
+        header('Location: /home/');
+  }
+  
+  public function deleteComment($id, $postId)
+  {
+        $this->commentManager->deleteComment($id);
+        header('Location: /post/'.$postId.'/');
+  }
+  
+  
+  public function flagOffComment($id)
+  {
+        $this->commentManager->flagOffComment($id);
+        header('Location: /profil/');
+  }
+    
+  public function logout()
+  {
+        $this->adminManager->logout();
+        header('Location: /home/');
+  }
+  
 }
