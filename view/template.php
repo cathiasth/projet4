5c40a4af127b889d504898e927a798c4d677eddf
Cathia <32756472+cathiasth@users.noreply.github.com>
2020-12-21 08:09:29 -0300

    Dernières modifications la vue template

--- view/template.php
+++ view/template.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
- <html>
-     <head>
-         <meta charset="utf-8" />
-         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <!-- Latest compiled and minified CSS -->
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-         <link rel="stylesheet" href="https://use.typekit.net/mrt8rvs.css">
+        <link rel="stylesheet" href="https://use.typekit.net/mrt8rvs.css">
 
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-         <link rel="stylesheet" type="text/css" href="../public/css/style.css" />
-         <link rel="stylesheet" type="text/css" href="../public/css/home.css" />
-         <link rel="stylesheet" type="text/css" href="../public/css/blog.css" />
-         <link rel="stylesheet" type="text/css" href="../public/css/header.css" />
-         <link rel="stylesheet" type="text/css" href="../public/css/footer.css" />
-         <link rel="stylesheet" type="text/css" href="../public/css/contact.css" />
-         <link rel="stylesheet" type="text/css" href="../public/css/editor.css" />
+        <link rel="stylesheet" type="text/css" href="/public/css/style.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/about.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/home.css" />
+        <link rel="stylesheet" type="text/css" href="/public/css/blog.css" />
+        <link rel="stylesheet" type="text/css" href="/public/css/header.css" />
+        <link rel="stylesheet" type="text/css" href="/public/css/footer.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/contact.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/editor.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/profil.css" />
+        <link rel="stylesheet" type="text/css" href="../public/css/mention.css" />
 
-         <title><?= $title ?></title>
+        <title><?= $title ?></title>
+        <script src="https://cdn.tiny.cloud/1/fw8jr1b40i2sa82rlhi6yya33ai0iobd5xi6ni8pcux4yxr0/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
-         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-             integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-             crossorigin="" />
-       <script src="https://cdn.tiny.cloud/1/fw8jr1b40i2sa82rlhi6yya33ai0iobd5xi6ni8pcux4yxr0/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-
-     </head>      
-     <body>
-         <?= $content ?>
-     </body>
- </html>
+    </head>      
+    <body>
+        <?= $content ?>
+    </body>
+</html>
